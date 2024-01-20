eebea62f92a11749c8f590c5ba5002f91ff2af6f
anddr13 <92887806+anddr13@users.noreply.github.com>
2024-01-20 11:03:28 +0200

    Update run_babylond.sh

--- run_babylond.sh
+++ run_babylond.sh
@@ -4,4 +4,4 @@
 BABYLOND="/usr/local/bin/babylond"
 
 # Run the babylond command and capture both stdout and stderr
-$BABYLOND tx epoching delegate bbnvaloper1ak6xmt8dvwxhsqwvz3mduwnplrl75j7dnlr0r5 1000000ubbn --from wallet --chain-id bbn-test-2 --gas-adjustment 1.4 --gas auto --fees 10ubbn -y
+$BABYLOND tx epoching delegate bbnvaloper1xazmjmvjh7vqw37mxmz545373ja7y8vp68xsdu 1000000ubbn --from wallet --chain-id bbn-test-2 --gas-adjustment 1.4 --gas auto --fees 10ubbn -y
